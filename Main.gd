5f176151113cb44b0d92c78d15d4ac1f8adf493b
Dan Brook <dan@broquaint.com>
2021-10-19 15:28:17 +0100

    Added a grid
    Only visual and possibly not the best implementation.

---
+++ Main.gd
@@ -0,0 +1,10 @@
+extends Node
+
+#onready var lines_vert  = []
+#onready var lines_horiz = []
+
+func _ready():
+	pass
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
